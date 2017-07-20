4cd23163cecaa723071e9d2a8640700a057e85c3
GAIUS8 <gaius827@gmail.com>
2017-07-20 11:39:51 +0900

    ebextensions 추가

--- django_app/member/models.py
+++ django_app/member/models.py
@@ -3,4 +3,7 @@ from django.db import models
 
 
 class MyUser(AbstractUser):
+    def __init__(self):
+        return self.username
+
     img_profile = models.ImageField(upload_to='user', blank=True)
