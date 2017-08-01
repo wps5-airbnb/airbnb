44218e1def59bc1d6808e27482957f0954a85cd6
Yunsik Park <space214@gmail.com>
2017-08-01 15:26:20 +0900

    UserRetrieveUpdateDestroyView - permission, apis, url추가

--- django_app/member/apis/users.py
+++ django_app/member/apis/users.py
@@ -1,10 +1,14 @@
-from rest_framework import generics
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
+from utils.permissions import ObjectIsRequestUser
 from ..models import MyUser
 from ..serializers import UserSerializer, UserCreateSerializer
 
 __all__ = [
     'UserCreateListView',
+    'UserRetrieveUpdateDestroyView',
 ]
 
 
@@ -26,4 +30,54 @@ class UserCreateListView(generics.ListCreateAPIView):
                 instance.img_profile = img_profile
                 instance.save()
                 break
+
+# class UserRetrieveUpdateDestroyView(APIView):
+#     permission_classes = (
+#         permissions.IsAuthenticatedOrReadOnly,
+#     )
+#     @staticmethod
+#     def get_object(pk):
+#         try:
+#             return MyUser.objects.get(pk=pk)
+#         except MyUser.DoesNotExist:
+#             raise Response(status.HTTP_404_NOT_FOUND)
+#
+#     # retrieve
+#     def get(self, request, pk):
+#         user = self.get_object(pk)
+#         serializer = UserSerializer(user)
+#         return Response(serializer.data)
+#
+#     # update
+#     def put(self, request, pk):
+#         user = self.get_object(pk)
+#         serializer = UserSerializer(user, request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     # partial update
+#     def patch(self, request, pk):
+#         user = self.get_object(pk)
+#         serializer = UserSerializer(user, request.data, partial=True)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     # destroy
+#     def delete(self, request, pk):
+#         user = self.get_object(pk)
+#         user.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class UserRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MyUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        ObjectIsRequestUser
+    )
 
