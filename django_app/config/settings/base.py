d93b9b610a8d74269f7968c84a8c7f8def80fc55
GAIUS8 <gaius827@gmail.com>
2017-08-06 16:33:43 +0900

    413 error solving test4, nginx settings변경

--- django_app/config/settings/base.py
+++ django_app/config/settings/base.py
@@ -126,15 +126,3 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     )
 }
-
-# django file settings
-CONTENT_TYPES = ['image', 'video']
-# 2.5MB - 2621440
-# 5MB - 5242880
-# 10MB - 10485760
-# 20MB - 20971520
-# 50MB - 5242880
-# 100MB 104857600
-# 250MB - 214958080
-# 500MB - 429916160
-MAX_UPLOAD_SIZE = "5242880"
