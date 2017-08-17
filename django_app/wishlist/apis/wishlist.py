7c141ec72dbbf5a433aa78895c360b6f3d9d1b45
GAIUS8 <gaius827@gmail.com>
2017-08-17 10:09:14 +0900

    Indentation

--- django_app/wishlist/apis/wishlist.py
+++ django_app/wishlist/apis/wishlist.py
@@ -1,5 +1,4 @@
 from rest_framework import authentication, status
-from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
