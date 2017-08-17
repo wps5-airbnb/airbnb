abffe8706769164046121fb6deeb991ecfa196a0
GAIUS8 <gaius827@gmail.com>
2017-08-17 13:51:17 +0900

    wishlist name 설정

--- django_app/wishlist/models/wishlist.py
+++ django_app/wishlist/models/wishlist.py
@@ -6,6 +6,9 @@ __all__ = [
 
 
 class Wishlist(models.Model):
+    def __str__(self):
+        return '{}의 pk={}하우스 좋아요'.format(self.liker.username, self.house.pk)
+
     liker = models.ForeignKey('member.MyUser', on_delete=models.CASCADE)
     house = models.ForeignKey('house.House', on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
