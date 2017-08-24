88b4d1f891b90b6c7a9e42b3fb46af0358a7a835
GAIUS8 <gaius827@gmail.com>
2017-08-24 12:36:10 +0900

    House Functional Test 작성 - house update test 작성

--- django_app/house/tests/house.py
+++ django_app/house/tests/house.py
@@ -7,7 +7,7 @@ from django.test import TestCase
 from django.test.client import encode_multipart
 from rest_framework.test import APIClient
 
-from house.models import House
+from house.models import House, Amenities
 from member.models import MyUser
 
 
@@ -67,6 +67,15 @@ class HouseAPITest(TestCase):
             latitude=12.1234,
             longitude=127.1234,
         )
+
+        # 테스트용 데이터베이스에 Amenities 셋팅
+        lists = ['Pets_allowed', 'Elevator', 'Gym', 'Indoor_fireplace', 'Internet',
+                 'Doorman', 'Kitchen', 'Pool', 'Smoking_allowed', 'Wheelchair_accessible',
+                 'Wireless_Internet', 'Free_parking', 'Breakfast', 'Dryer', 'Cable_TV', 'Hangers',
+                 'Washer', 'Shampoo', 'Essentials', 'Heating', 'TV', 'Air_conditioning', ]
+
+        for item in lists:
+            Amenities.objects.create(name=item)
 
     def test_house_list(self):
         """
@@ -166,3 +175,104 @@ class HouseAPITest(TestCase):
         request_image_url = '../.media/user/jeakyung.jpg'
         for image in house.image.all():
             self.assertTrue(filecmp.cmp(request_image_url, '../.media/{}'.format(image.image)))
+
+    def test_house_update(self):
+        """
+        시나리오
+            image -> 기존의 사진은 그대로 두고 다른 사진 2장을 추가하는 상황
+            숙소의 이름 변경
+            숙소에 대한 소개 및 내용 추가
+            숙소 편의시설(Amenities) 추가
+        :return:
+        """
+
+        request_image1 = open('../.media/HouseImage/suji2.jpg', 'rb')  # image를 binary형식으로 open 후 저장
+        request_image2 = open('../.media/HouseImage/suji3.jpg', 'rb')  # image를 binary형식으로 open 후 저장
+
+        house_content = {
+            'title': '변경된 숙소의 제목',
+            'price_per_day': 80000,
+            'extra_people_fee': 30000,
+            'cleaning_fee': 10000,
+            'weekly_discount': 7,
+            'accommodates': 3,
+            'bathrooms': 1,
+            'bedrooms': 1,
+            'beds': 1,
+            'latitude': 12.1234,
+            'longitude': 127.1234,
+            'image1': request_image1,
+            'image2': request_image2,
+            'address': '서울시 강남구 신사역 근처',
+            'introduce': '서울시내 한복판에 있어서 교통이 좋고, 접근성이 좋습니다.',
+            'space_info': '숙소 내 모든 시설을 제한없이 이용할 수 있습니다.',
+            'guest_access': '내부에 애완동물과 같이 올 수 있습니다.',
+            'amenities': 'TV, Internet, Pool'
+        }
+
+        encoded_content = encode_multipart('BoUnDaRyStRiNg', house_content)
+        content_type = 'multipart/form-data; boundary=BoUnDaRyStRiNg'
+
+        response = self.client.patch(
+            '/apis/house/1/',
+            encoded_content,
+            content_type=content_type,
+        )
+
+        request_image1.close()
+        request_image2.close()
+
+        result = response.json()
+
+        self.assertEqual(response.status_code, 200)
+
+        # Response Check
+        self.assertEqual(result['host']['pk'], self.user.pk)
+        self.assertEqual(result['title'], house_content['title'])
+        self.assertEqual(result['price_per_day'], house_content['price_per_day'])
+        self.assertEqual(result['extra_people_fee'], house_content['extra_people_fee'])
+        self.assertEqual(result['cleaning_fee'], house_content['cleaning_fee'])
+        self.assertEqual(result['weekly_discount'], house_content['weekly_discount'])
+        self.assertEqual(result['accommodates'], house_content['accommodates'])
+        self.assertEqual(result['bathrooms'], house_content['bathrooms'])
+        self.assertEqual(result['bedrooms'], house_content['bedrooms'])
+        self.assertEqual(result['beds'], house_content['beds'])
+        self.assertEqual(result['latitude'], house_content['latitude'])
+        self.assertEqual(result['longitude'], house_content['longitude'])
+        self.assertEqual(result['address'], house_content['address'])
+        self.assertEqual(result['introduce'], house_content['introduce'])
+        self.assertEqual(result['space_info'], house_content['space_info'])
+        self.assertEqual(result['guest_access'], house_content['guest_access'])
+
+        #  House객체가 원래 의도대로 잘 변경되었는지 검증하는 부분
+        house = House.objects.get(pk=result['pk'])
+
+        self.assertEqual(result['title'], house.title)
+        self.assertEqual(result['price_per_day'], house.price_per_day)
+        self.assertEqual(result['extra_people_fee'], house.extra_people_fee)
+        self.assertEqual(result['cleaning_fee'], house.cleaning_fee)
+        self.assertEqual(result['weekly_discount'], house.weekly_discount)
+        self.assertEqual(result['accommodates'], house.accommodates)
+        self.assertEqual(result['bathrooms'], house.bathrooms)
+        self.assertEqual(result['bedrooms'], house.bedrooms)
+        self.assertEqual(result['beds'], house.beds)
+        self.assertEqual(result['latitude'], house.latitude)
+        self.assertEqual(result['longitude'], house.longitude)
+        self.assertEqual(result['address'], house.address)
+        self.assertEqual(result['introduce'], house.introduce)
+        self.assertEqual(result['space_info'], house.space_info)
+        self.assertEqual(result['guest_access'], house.guest_access)
+
+        # Amenities가 정상적으로 들어갔는지 검증하는 부분
+        request_amenities = [i.strip() for i in house_content['amenities'].split(',')].sort()
+        created_amenities = [i.name for i in house.amenities.all()].sort()
+
+        self.assertEqual(request_amenities, created_amenities)
+
+        # Image가 정상적으로 추가되었는지 검증하는 부분
+        request_image_url1 = '../.media/HouseImage/suji2.jpg'
+        request_image_url2 = '../.media/HouseImage/suji3.jpg'
+        created_images = ['../.media/{}'.format(url.image) for url in house.image.all()]
+
+        self.assertTrue(filecmp.cmp(request_image_url1, created_images[0]))
+        self.assertTrue(filecmp.cmp(request_image_url2, created_images[1]))
